751a740c8faeaa817a8469856a1227ecd5617c25
Igor Maguskin <maguskin.igor@gmail.com>
2020-08-26 00:30:20 +0100

    Fix the CSS of the navigation items

--- src/components/Navigation/NavigationItems/NavigationItem/styled.js
+++ src/components/Navigation/NavigationItems/NavigationItem/styled.js
@@ -1,26 +1,42 @@
 import styled from "styled-components";
 
 export const NavigationItem = styled.li`
-  margin: 0;
-  box-sizing: border-box;
-  display: flex;
-  height: 100%;
-  align-items: center;
+  margin: 10px 0;
+  display: block;
+  width: 100%;
   
   & a {
-    color: white;
+    color: #8F5C2C;
     text-decoration: none;
-    height: 100%;
-    padding: 16px 10px;
-    border-bottom: ${props => props.active ? "4px solid #40A4C8" : "4px solid transparent"}
+    width: 100%;
     box-sizing: border-box;
     display: block;
-    background-color: ${props => props.active ? "#8F5C2C" : ""}
+    background-color: ${props => props.active ? "#8F5C2C" : ""};
   }
   
   & a:hover, a:active, a.active {
-    background-color: #8F5C2C;
-    border-bottom: 4px solid #40A4C8;
-    color: white;
+    color: #40A4C8;
+  }
+  
+  @media (min-width: 500px) {
+    margin: 0;
+    display: flex;
+    height: 100%;
+    width: auto;
+    align-items: center;
+    
+    & a {
+      color: white;
+      height: 100%;
+      padding: 16px 10px;
+      border-bottom: ${props => props.active ? "4px solid #40A4C8" : "4px solid transparent"};
+      background-color: ${props => props.active ? "#8F5C2C" : ""};
+    }
+    
+    & a:hover, a:active, a.active {
+      background-color: #8F5C2C;
+      border-bottom: 4px solid #40A4C8;
+      color: white;
+    }
   }
 `;
