60767559d8e7dd51a35008a592f324a1c2bb9835
Igor Maguskin <maguskin.igor@gmail.com>
2020-10-22 18:10:50 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
+import {Route, Switch} from "react-router-dom";
 
 const app = () => (
   <div>
     <Layout>
-      <BurgerBuilder/>
-      <Checkout/>
+      <Switch>
+        <Route path="/checkout" component={Checkout}/>
+        <Route path="/" exact component={BurgerBuilder}/>
+      </Switch>
     </Layout>
   </div>
 );
