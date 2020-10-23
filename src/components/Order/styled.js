2df4d49f0e84fdbee01c7a285ef4fecc7a57f78d
Igor Maguskin <maguskin.igor@gmail.com>
2020-10-23 01:11:35 +0100

    Fetch orders

--- src/components/Order/styled.js
+++ src/components/Order/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Order = styled.div`
-  width: 100%;
+  width: 80%;
   border: 1px solid #eee;
   box-shadow: 0 2px 3px #ccc;
   padding: 10px;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 const Orders = () => {
+
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('/orders.json')
+      .then(response => {
+        console.log(response.data);
+        const fetchedOrders = Object.keys(response.data).map(i => (
+          {
+            ...response.data[i],
+            id: i
+          }
+        ));
+        setLoading(false);
+        setOrders(fetchedOrders);
+      })
+      .catch(() => setLoading(false));
+  }, []);
+
   return (
     <div>
       <Order/>
@@ -10,4 +32,4 @@ const Orders = () => {
   );
 };
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
