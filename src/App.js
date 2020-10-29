edbbdfd513862452a0ee5598546a5f6f9a0b2d4e
Igor Maguskin <maguskin.igor@gmail.com>
2020-10-29 22:53:19 +0000

    Protect routes with guards

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import Layout from 'hoc/Layout/Layout'
 import BurgerBuilder from 'containers/BurgerBuilder/BurgerBuilder'
 import Checkout from 'containers/Checkout/Checkout'
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Orders from 'containers/Orders/Orders'
 import Auth from 'containers/Auth/Auth'
 import Logout from 'containers/Auth/Logout/Logout'
@@ -11,29 +11,41 @@ import { bindActionCreators } from 'redux'
 import { actions } from 'containers/Auth/authSlice'
 import { withRouter } from 'react-router'
 
-const App = ({ initAuth }) => {
+const App = ({ initAuth, signedIn }) => {
   useEffect(() => {
     initAuth()
   }, [initAuth])
 
+  const routes = signedIn ? (
+    <Switch>
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/orders" component={Orders} />
+      <Route path="/logout" component={Logout} />
+      <Route path="/auth" component={Auth} />
+      <Route path="/" exact component={BurgerBuilder} />
+    </Switch>
+  ) : (
+    <Switch>
+      <Route path="/auth" component={Auth} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  )
+
   return (
     <div>
-      <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" exact component={BurgerBuilder} />
-        </Switch>
-      </Layout>
+      <Layout>{routes}</Layout>
     </div>
   )
 }
+
+const mapStateToProps = (state) => ({
+  signedIn: state.auth.token !== null,
+})
 
 const mapDispatchToProps = (dispatch) => {
   const { initAuth } = bindActionCreators(actions, dispatch)
   return { initAuth }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App))
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
